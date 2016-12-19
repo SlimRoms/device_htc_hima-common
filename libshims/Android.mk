6b7dc99d99103edbd182794a28da792ae5bed67e
Christopher R. Palmer <crpalmer@gmail.com>
2016-12-19 03:31:00 -0600

    hima: ril: Shim a missing symbol
    Change-Id: Ic9b52f8449d7234d011a9175141209636c1d8dd4

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -13,3 +13,14 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MULTILIB := 32
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    htc_ril.c
+
+LOCAL_MODULE := libshim_ril
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+
+include $(BUILD_SHARED_LIBRARY)
